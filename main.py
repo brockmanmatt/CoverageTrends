69f611d1d2ea84b76c7c756d9a5e7c764a9b787d
Matt Brockman <brockman@email.arizona.edu>
2020-05-18 15:32:48 -0700

    "added cycle"

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import git
+import git, schedule, time
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 
-g = git.cmd.Git(".")
-g.pull()
-print("pulled")
+
+def updateRepo():
+    git.cmd.Git(".").pull()
+    print("pulled")
+
+def cycle():
+    updateRepo()
+    print("test")
+
+schedule.every(10).seconds.do(cycle)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
