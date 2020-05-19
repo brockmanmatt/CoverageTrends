0f686cf86bb6caafd40e7a01bdf6ab5f829d7679
Matt Brockman <brockman@email.arizona.edu>
2020-05-19 13:40:59 -0700

    cnn try a few times

--- parsers/cnn.py
+++ parsers/cnn.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import sys, requests
+import sys, requests, time
 
 def notBadURL(url):
 	if url.startswith("/vi"):
@@ -18,36 +18,40 @@ k, cnn's officially wierd
 """
 def scrape():
 
-	results = {}
-	url = "http://www.cnn.com/"
-	page = requests.get(url)
-	soup = BeautifulSoup(page.text, 'html.parser')
+	for i in range (4):
+		try:
+			results = {}
+			url = "http://www.cnn.com/"
+			page = requests.get(url)
+			soup = BeautifulSoup(page.text, 'html.parser')
 
-	currentContainer = soup.find_all("script")
-	for script in currentContainer:
-		if (script.text.find("articleList") > -1):
-			urls = script.text.split("\"uri\":")
-			urls = urls[1:]
+			currentContainer = soup.find_all("script")
+			for script in currentContainer:
+				if (script.text.find("articleList") > -1):
+					urls = script.text.split("\"uri\":")
+					urls = urls[1:]
 
-			try:
-				urls[len(urls)-1] = urls[len(urls)-1][:urls[len(urls)-1].find("\",\"layout")]
-			except:
-				continue
-			for url in urls:
-				myURL = url[1:url.find(".html")+5]
+					try:
+						urls[len(urls)-1] = urls[len(urls)-1][:urls[len(urls)-1].find("\",\"layout")]
+					except:
+						continue
+					for url in urls:
+						myURL = url[1:url.find(".html")+5]
 
-				if not notBadURL(myURL):
-					continue
+						if not notBadURL(myURL):
+							continue
 
-				headline = url[url.find("headline")+11:url.find("\",\"thumbnail")]
-				if headline[0] == "\\":
-					headline = headline[13:len(headline)-14]
-				results[myURL] = {}
-				results[myURL]["text"] = headline
-				results[myURL]["notes"] = ""
+						headline = url[url.find("headline")+11:url.find("\",\"thumbnail")]
+						if headline[0] == "\\":
+							headline = headline[13:len(headline)-14]
+						results[myURL] = {}
+						results[myURL]["text"] = headline
+						results[myURL]["notes"] = ""
 
 
-	return results
+			return results
+		except:
+			time.sleep(5)
 
 if __name__ == '__main__':
 	run()
