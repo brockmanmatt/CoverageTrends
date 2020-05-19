cc7ce6726e9f3c734aa6616d54d14d52a0c861cd
Matt Brockman <brockman@email.arizona.edu>
2020-05-18 21:26:01 -0700

    fixing

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from bs4 import BeautifulSoup as bs
-import time, datetime
+import time, datetime, os
 
 from datetime import timezone
 
@@ -15,17 +15,19 @@ def run():
 
     for _, row in sources.T.iteritems():
         if "{}.py".format(row["Parser"]) in os.listdir("parsers"):
-            module = "parsers." + row["Parser"]
-            module = importlib.import_module(module)
-            tmp = pd.DataFrame(module.scrape()).T
-            tmp["date"] = myTime
+            try:
+                module = "parsers." + row["Parser"]
+                module = importlib.import_module(module)
+                tmp = pd.DataFrame(module.scrape()).T
+                tmp["date"] = myTime
 
-            os.makedirs("archived_links/{}/{}/".format(row["Parser"], myMonth), exist_ok=True)
+                os.makedirs("archived_links/{}/{}/".format(row["Parser"], myMonth), exist_ok=True)
 
-            if not os.path.isfile("{}/{}/{}/{}_{}.csv".format("archived_links/", row["Parser"], myMonth,row["Parser"], myDate)):
-                tmp.to_csv("{}/{}/{}/{}_{}.csv".format("archived_links/", row["Parser"], myMonth,row["Parser"], myDate))
-            else:
-                tmp.to_csv("{}/{}/{}/{}_{}.csv".format("archived_links/", row["Parser"], myMonth,row["Parser"], myDate), mode='a', header=False)
-
+                if not os.path.isfile("{}/{}/{}/{}_{}.csv".format("archived_links/", row["Parser"], myMonth,row["Parser"], myDate)):
+                    tmp.to_csv("{}/{}/{}/{}_{}.csv".format("archived_links/", row["Parser"], myMonth,row["Parser"], myDate))
+                else:
+                    tmp.to_csv("{}/{}/{}/{}_{}.csv".format("archived_links/", row["Parser"], myMonth,row["Parser"], myDate), mode='a', header=False)
+            except:
+                pass
 if __name__ == '__main__':
     run()
