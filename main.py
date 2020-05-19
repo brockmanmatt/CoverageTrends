59a1aff12d19647e101fdd24d6892e96b8aa630c
Matt Brockman <brockman@email.arizona.edu>
2020-05-19 13:40:01 -0700

    making it every hour instead of every 4

--- main.py
+++ main.py
@@ -26,13 +26,29 @@ def git_push(message='auto-update'):
 os.makedirs("archived_links", exist_ok=True)
 
 schedule.every().day.at("00:00").do(cycle)
+schedule.every().day.at("01:00").do(cycle)
+schedule.every().day.at("02:00").do(cycle)
+schedule.every().day.at("03:00").do(cycle)
 schedule.every().day.at("04:00").do(cycle)
+schedule.every().day.at("05:00").do(cycle)
+schedule.every().day.at("06:00").do(cycle)
+schedule.every().day.at("07:00").do(cycle)
 schedule.every().day.at("08:00").do(cycle)
+schedule.every().day.at("09:00").do(cycle)
+schedule.every().day.at("10:00").do(cycle)
+schedule.every().day.at("11:00").do(cycle)
 schedule.every().day.at("12:00").do(cycle)
+schedule.every().day.at("13:00").do(cycle)
+schedule.every().day.at("14:00").do(cycle)
+schedule.every().day.at("15:00").do(cycle)
 schedule.every().day.at("16:00").do(cycle)
+schedule.every().day.at("17:00").do(cycle)
+schedule.every().day.at("18:00").do(cycle)
+schedule.every().day.at("19:00").do(cycle)
 schedule.every().day.at("20:00").do(cycle)
-
-cycle()
+schedule.every().day.at("21:00").do(cycle)
+schedule.every().day.at("22:00").do(cycle)
+schedule.every().day.at("23:00").do(cycle)
 
 while True:
     try:
