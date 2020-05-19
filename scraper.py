a4f07b206a70e87c312b01d38d09d295f28b8dbd
Matt Brockman <brockman@email.arizona.edu>
2020-05-18 22:27:14 -0700

    fixed bug

--- scraper.py
+++ scraper.py
@@ -1,17 +1,20 @@
 import pandas as pd
 from bs4 import BeautifulSoup as bs
-import time, datetime, os
+import time, datetime, os, importlib
 
 from datetime import timezone
 
 from parsers import *
 
-def run():
+def run(verbose=False):
     myTime = datetime.datetime.now(tz=timezone.utc).strftime('%Y%m%d-%H00')
     myDate = datetime.datetime.now(tz=timezone.utc).strftime('%Y%m%d')
     myMonth = myDate = datetime.datetime.now(tz=timezone.utc).strftime('%Y%m')
 
     sources = pd.read_excel("NewsSites.xlsx")
+
+    if verbose:
+        print("checking rows")
 
     for _, row in sources.T.iteritems():
         if "{}.py".format(row["Parser"]) in os.listdir("parsers"):
@@ -27,7 +30,9 @@ def run():
                     tmp.to_csv("{}/{}/{}/{}_{}.csv".format("archived_links/", row["Parser"], myMonth,row["Parser"], myDate))
                 else:
                     tmp.to_csv("{}/{}/{}/{}_{}.csv".format("archived_links/", row["Parser"], myMonth,row["Parser"], myDate), mode='a', header=False)
-            except:
+            except Exception as e:
+                if verbose:
+                    print(e)
                 pass
 if __name__ == '__main__':
     run()
