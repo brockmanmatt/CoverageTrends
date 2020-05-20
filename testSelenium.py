dec6e14addc00374dfcb1a13b4b14f75b7deb7fd
Matt Brockman <brockman@email.arizona.edu>
2020-05-20 01:06:45 -0700

    more options

--- testSelenium.py
+++ testSelenium.py
@@ -1,12 +1,15 @@
 import time, os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import
+DesiredCapabilities
+
 
 # "$ xvfb-run python test.py", this is how you run this script
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(options=chrome_options)
-options=chrome_options
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(options=options)
 
 # Scraping steps
 driver.get("cnn.com")
