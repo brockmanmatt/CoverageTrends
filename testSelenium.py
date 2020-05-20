914432bc1c3756335b1a8408b88f8d70dbc71c41
Matt Brockman <brockman@email.arizona.edu>
2020-05-20 01:02:25 -0700

    testing selenium

---
+++ testSelenium.py
@@ -0,0 +1,15 @@
+import timem i
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# "$ xvfb-run python test.py", this is how you run this script
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(options=chrome_options)
+options=chrome_options
+
+# Scraping steps
+driver.get("cnn.com")
+time.sleep(3)
+print('Finished!')
+driver.quit()
