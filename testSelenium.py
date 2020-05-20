2a02d4135d6e87fbc439dffb734d9f9b915343f3
Matt Brockman <brockman@email.arizona.edu>
2020-05-20 01:07:13 -0700

    asdf

--- testSelenium.py
+++ testSelenium.py
@@ -1,8 +1,6 @@
 import time, os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import
-DesiredCapabilities
 
 
 # "$ xvfb-run python test.py", this is how you run this script
