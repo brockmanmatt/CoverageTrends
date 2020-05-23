b8c3adbf29cf4bcc3ab68033a950ae92e28ee26b
brockmanmatt <brockman#email.arizona.edu>
2020-05-23 00:07:32 +0000

    something broke on update

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-document.getElementById("start").innerHTML = "Did This Work?"
-
+var allowedFromDates = {}
+allowedFromDates['20200523-0000'] = ['china', 'case', 'biden', 'black', 'pandem', 'reopen']
+var allowedFromTopic = {}
+allowedFromTopic['pandem'] = ['20200523-0000']
+allowedFromTopic['biden'] = ['20200522-2140', '20200522-2200', '20200522-2230', '20200522-2300', '20200523-0000']
+allowedFromTopic['china'] = ['20200523-0000']
+allowedFromTopic['case'] = ['20200523-0000']
+allowedFromTopic['black'] = ['20200523-0000']
+allowedFromTopic['reopen'] = ['20200523-0000']
+const dates = [
+'20200523-0000'
+]
+const topics = [
+'pandem','biden','china','case','black','reopen'
+]
+function setupImgBox(){
+    var time = document.getElementById("timeButton").value;
+    var issue=document.getElementById("issueButton").value;
+    img_name = time + "_" + issue + ".jpg";
+    var newHTML = '<img src = "./img/';
+    newHTML += img_name;
+    newHTML += '" width=90%>';
+    document.getElementById("imgBox").innerHTML = newHTML;
+};
+function setupDropdownBox(){
+    newHTML = '<table id="SelectTable">'
+    newHTML += '<caption><i>Select a Series</i></caption>'
+    newHTML += '<tr><th>Datetime</th><th>Issue</th><th></tr></tr>'
+    newHTML += '<tr><td><select id="timeButton" onchange="setupImgBox()">';
+    dates.forEach(time => newHTML+= '<option value='+time+'>'+time+'</option>');
+    newHTML+= '</select></td>';
+    newHTML += '<td><select id="issueButton" onchange="setupImgBox()">';
+    topics.forEach(topic => newHTML+= '<option value='+topic+'>'+topic+'</option>');
+    newHTML += '</select></td></tr></table>';
+    document.getElementById("dropdowns").innerHTML = newHTML;
+};
