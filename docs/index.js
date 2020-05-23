2e35289149c31b3f49cd0e3c649ed0078e1e2be2
brockmanmatt <"brockman#email.arizona.edu">
2020-05-23 08:31:19 +0000

    auto-update

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,5 @@
 const topics = [
-'reopen_20200523-0800','pandem_20200523-0800','china_20200523-0800','black_20200523-0800','biden_20200523-0800','open_20200523-0330','open_20200523-0300','open_20200523-0230','case_20200523-0000','coronavirus_20200522-2100'
+'reopen_20200523-0830','pandem_20200523-0830','china_20200523-0830','black_20200523-0830','biden_20200523-0830','open_20200523-0330','open_20200523-0300','open_20200523-0230','case_20200523-0000','coronavirus_20200522-2100'
 ]
 function setupImgBox(){
     var myToken=document.getElementById("issueButton").value;
