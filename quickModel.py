805c11e099f20214cc494a698e472cfabd06d6d2
Matt Brockman <brockman@email.arizona.edu>
2020-05-23 23:15:31 -0700

    whoops, cleared code so quickModel blank

--- quickModel.py
+++ quickModel.py
@@ -1,15 +1,1 @@
-import multiprocessing, time
-
-def print_i():
-    i = 0
-    while True:
-        print (i)
-        i+=1
-        time.sleep(1)
-
-print("starting")
-process = multiprocessing.Process(target=print_i)
-process.start()
-
-time.sleep(30)
-process.terminate()
+print("loaded")
