9d14008ff24c30ba9fbf04c507960636a2459d0b
brockmanmatt <"brockman#email.arizona.edu">
2020-05-25 03:04:11 +0000

    auto-update

--- docs/index.js
+++ docs/index.js
@@ -1,11 +1,11 @@
 const topics = [
-'weekend_20200525-0230','virus_20200525-0230','trump_20200525-0230','reopen_20200525-0230','memori_20200525-0230','man_20200525-0230','coronavirus_20200525-0230','case_20200525-0010','biden_20200524-2130','amid_20200524-2000','pandem_20200524-1930','world_20200524-1800','black_20200524-1430','ain_20200524-1010','summer_20200523-2130','covid_20200523-1400','time_20200523-1330','beach_20200523-1230','china_20200523-1100','open_20200523-0330','open_20200523-0300','open_20200523-0230'
+'weekend_20200525-0300','virus_20200525-0300','trump_20200525-0300','reopen_20200525-0300','memori_20200525-0300','case_20200525-0300','man_20200525-0230','biden_20200524-2130','amid_20200524-2000','pandem_20200524-1930','world_20200524-1800','black_20200524-1430','ain_20200524-1010','summer_20200523-2130','covid_20200523-1400','time_20200523-1330','beach_20200523-1230','china_20200523-1100','open_20200523-0330','open_20200523-0300','open_20200523-0230'
 ]
 const VARtopics = [
-'world','amid','reopen','case','pandem','black','virus','man','coronavirus','biden','weekend','trump','memori','ain'
+'coronavirus','trump','black','ain','world','memori','biden','pandem','case','reopen','amid','virus','weekend','man'
 ]
 const SARIMAXtopics = [
-'reopen','case','virus','man','coronavirus','weekend','trump','memori'
+'coronavirus','memori','trump','case','virus','weekend','reopen','man'
 ]
 function setupImgBox(){
     var myToken=document.getElementById("issueButton").value;
