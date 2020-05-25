6c5ae690f0b4173245636eb392e66f236ed3675f
Matt Brockman <brockman@email.arizona.edu>
2020-05-24 19:39:00 -0700

    fixed bug in javascript

--- docs/index.js
+++ docs/index.js
@@ -2,10 +2,10 @@ const topics = [
 'weekend_20200525-0230','virus_20200525-0230','trump_20200525-0230','reopen_20200525-0230','memori_20200525-0230','man_20200525-0230','coronavirus_20200525-0230','case_20200525-0010','biden_20200524-2130','amid_20200524-2000','pandem_20200524-1930','world_20200524-1800','black_20200524-1430','ain_20200524-1010','summer_20200523-2130','covid_20200523-1400','time_20200523-1330','beach_20200523-1230','china_20200523-1100','open_20200523-0330','open_20200523-0300','open_20200523-0230'
 ]
 const VARtopics = [
-'coronavirus','trump','black','ain','world','memori','biden','pandem','case','reopen','amid','virus','weekend','man'
+'world','amid','reopen','case','pandem','black','virus','man','coronavirus','biden','weekend','trump','memori','ain'
 ]
 const SARIMAXtopics = [
-'coronavirus','memori','trump','case','virus','weekend','reopen','man'
+'reopen','case','virus','man','coronavirus','weekend','trump','memori'
 ]
 function setupImgBox(){
     var myToken=document.getElementById("issueButton").value;
@@ -48,7 +48,7 @@ function setupDropdownBox(){
     newHTML += '<td><select id="VARButton" onchange="setupVARImgBox()">';
     VARtopics.forEach(topic => newHTML+= '<option value='+topic+'>'+topic+'</option>');
     newHTML += '</select></td>'
-    newHTML += '<td><select id="SARIMAXButton" onchange="setupVARImgBox()">';
+    newHTML += '<td><select id="SARIMAXButton" onchange="setupSARIMAXImageBox()">';
     SARIMAXtopics.forEach(topic => newHTML+= '<option value='+topic+'>'+topic+'</option>');
     newHTML += '</select></td>'
     newHTML += '</tr></table>';
