79038ceb08e4ebad97f045703ade9324bfd34dee
Matt Brockman <brockman@email.arizona.edu>
2020-05-27 02:35:10 -0700

    manual run add-models (515.0230128765106 seconds taken)

--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,14 @@
 const topics = [
-'trump_20200527-0050','mask_20200527-0050','covid_20200527-0050','coronavirus_20200527-0050','biden_20200527-0050','american_20200527-0050','trump_20200526-2110','reopen_20200526-2110','presid_20200526-2110','mask_20200526-2110','covid_20200526-2110','coronavirus_20200526-2110','trump_20200526-1100','test_20200526-1100','reopen_20200526-1100','pandem_20200526-1100','memori_20200526-1100','covid_20200526-1100','coronavirus_20200526-1100','biden_20200526-1100','american_20200526-1100','trump_20200526-1040','reopen_20200526-1040','pandem_20200526-1040','memori_20200526-1040','covid_20200526-1040','coronavirus_20200526-1040','biden_20200526-1040','american_20200526-1040','home_20200526-0610','summer_20200526-0530','face_20200526-0430','year_20200526-0330','virus_20200525-2230','live_20200525-2230','honor_20200525-2100','weekend_20200525-2030','carolina_20200525-1930','penc_20200525-1830','nation_20200525-1730','amid_20200525-1600','case_20200525-1530','know_20200525-1430','make_20200525-1330','black_20200525-1230','death_20200525-1000','florida_20200525-0830','world_20200524-1800','ain_20200524-1010','time_20200523-1330','beach_20200523-1230','china_20200523-1100'
+'trump_20200527-0920','polic_20200527-0920','plan_20200527-0920','nurs_20200527-0920','mask_20200527-0920','mail_20200527-0920','covid_20200527-0920','coronavirus_20200527-0920','case_20200527-0920','biden_20200527-0920','vote_20200527-0900','home_20200527-0900','american_20200527-0050','reopen_20200526-2110','presid_20200526-2110','test_20200526-1100','reopen_20200526-1100','pandem_20200526-1100','memori_20200526-1100','american_20200526-1100','reopen_20200526-1040','pandem_20200526-1040','memori_20200526-1040','american_20200526-1040','home_20200526-0610','summer_20200526-0530','face_20200526-0430','year_20200526-0330','virus_20200525-2230','live_20200525-2230','honor_20200525-2100','weekend_20200525-2030','carolina_20200525-1930','penc_20200525-1830','nation_20200525-1730','amid_20200525-1600','know_20200525-1430','make_20200525-1330','black_20200525-1230','death_20200525-1000','florida_20200525-0830','world_20200524-1800','ain_20200524-1010','time_20200523-1330','beach_20200523-1230','china_20200523-1100'
 ]
 const VARtopics = [
-'world','summer','amid','american','reopen','make','case','year','home','pandem','know','face','black','virus','test','coronavirus','biden','nation','weekend','covid','honor','death','mask','trump','florida','live','memori','ain'
+'world','summer','amid','american','reopen','make','case','year','home','pandem','know','face','black','virus','polic','test','vote_and_mail','coronavirus','biden','nation','weekend','covid','home_and_nurs','nurs_and_home','honor','mail_and_vote','death','plan','mask','trump','florida','live','memori','ain'
 ]
 const SARIMAXtopics = [
-'summer','amid','american','reopen','make','case','year','home','pandem','know','face','black','virus','test','coronavirus','biden','nation','weekend','covid','honor','death','mask','trump','florida','live','memori'
+'summer','amid','american','reopen','make','case','year','home','pandem','know','face','black','virus','polic','test','vote_and_mail','coronavirus','biden','nation','weekend','covid','home_and_nurs','nurs_and_home','honor','mail_and_vote','death','plan','mask','trump','florida','live','memori'
 ]
 const CORRELATIONtopics = [
-'memori','trump','mask','coronavirus','biden','test','covid','year','face','home','pandem','american','summer','reopen'
+'mail_and_vote','memori','trump','mask','plan','coronavirus','biden','vote_and_mail','test','nurs_and_home','home_and_nurs','covid','year','polic','face','home','pandem','american','summer','case','reopen'
 ]
 function setupImgBox(){
     var myToken=document.getElementById("issueButton").value;
